92658ba30b862be3c9fd0e88079fcaa6406b68a8
sibelephant <masterpiekhalid@gmail.com>
2025-05-14 13:05:49 +0100

    supplementary 1

---
+++ src/components/PostMenuActions.jsx
@@ -0,0 +1,42 @@
+const PostMenuActions = () => {
+  return (
+    <div className="">
+      <h1>Actions</h1>
+      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 48 48"
+          width="20px"
+          height="20px"
+        >
+          <path
+            d="M12 4C10.3 4 9 5.3 9 7v34l15-9 15 9V7c0-1.7-1.3-3-3-3H12z"
+            stroke="black"
+            strokeWidth="2"
+          />
+        </svg>
+        <span>Save this Post</span>
+      </div>
+
+      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer hover:text-red-600">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 48 48"
+          width="20px"
+          height="20px"
+        >
+          <path
+            d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4V14H12v24zM38 8h-7l-2-2H19l-2 2h-7v4h28V8z"
+            stroke="red"
+            color="red"
+            strokeWidth="2"
+            fill="none"
+          />
+        </svg>
+        <span>Delete this Post</span>
+      </div>
+    </div>
+  );
+};
+
+export default PostMenuActions;
--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,9 +1,115 @@
-
+import { Link } from "react-router-dom";
+import Image from "../components/Image";
+import PostMenuActions from "../components/PostMenuActions";
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostpage</div>
-  )
-}
+    <div className="flex flex-col gap-8 ">
+      {/* detail */}
+      <div className="flex gap-8">
+        <div className="lg:w-3/5 flex flex-col gap-8 ">
+          <h1 className="lg:text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <Link className="text-blue-800">Khalid Ibrahim</Link>
+            <span>on</span>
+            <Link className="text-blue-800">Web Design</Link>
+            <span>2 days ago</span>
+          </div>
+          <p className="text-gray-500 font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque illo
+            incidunt veniam tempora voluptatibus facilis quia expedita totam,
+            dolorem ab minima cupiditate asperiores nostrum quidem et maxime
+            possimus quod accusamus?
+          </p>
+        </div>
+        <div className="hidden lg:block w-2/5 ">
+          <Image src="postImg.jpeg" w="600" className="rounded-2xl" />
+        </div>
+      </div>
+      {/* content */}
+      <div className="flex flex-col md:flex-row gap-8">
+        {/* text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            nostrum minus blanditiis, perferendis natus, accusamus praesentium
+            laudantium officia itaque corporis ullam a quasi obcaecati deserunt
+            consectetur mollitia debitis! Impedit, quas! Dolores voluptates
+            dicta, impedit odit laborum possimus cumque voluptas modi dolorum.
+            Aliquid accusantium exercitationem ex dolorem quis quae, enim, porro
+            labore voluptatum ipsa animi sint quam id vero nesciunt unde.
+            Commodi, sapiente obcaecati ab, fuga consequatur voluptatibus hic
+            voluptatum cumque neque deleniti minima culpa reprehenderit
+            praesentium sint quam. Harum saepe dignissimos fugit aperiam eius
+            fugiat ipsum. Modi nulla dolorum voluptates. Reiciendis odit ut
+            nihil, tempore blanditiis quisquam laboriosam unde nisi rem, illo
+            rerum accusantium atque nobis voluptates laborum! Id voluptas
+            quaerat velit sed dolore eligendi voluptatem architecto obcaecati
+            nostrum itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            nostrum minus blanditiis, perferendis natus, accusamus praesentium
+            laudantium officia itaque corporis ullam a quasi obcaecati deserunt
+            consectetur mollitia debitis! Impedit, quas! Dolores voluptates
+            dicta, impedit odit laborum possimus cumque voluptas modi dolorum.
+            Aliquid accusantium exercitationem ex dolorem quis quae, enim, porro
+            labore voluptatum ipsa animi sint quam id vero nesciunt unde.
+            Commodi, sapiente obcaecati ab, fuga consequatur voluptatibus hic
+            voluptatum cumque neque deleniti minima culpa reprehenderit
+            praesentium sint quam. Harum saepe dignissimos fugit aperiam eius
+            fugiat ipsum. Modi nulla dolorum voluptates. Reiciendis odit ut
+            nihil, tempore blanditiis quisquam laboriosam unde nisi rem, illo
+            rerum accusantium atque nobis voluptates laborum! Id voluptas
+            quaerat velit sed dolore eligendi voluptatem architecto obcaecati
+            nostrum itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            nostrum minus blanditiis, perferendis natus, accusamus praesentium
+            laudantium officia itaque corporis ullam a quasi obcaecati deserunt
+            consectetur mollitia debitis! Impedit, quas! Dolores voluptates
+            dicta, impedit odit laborum possimus cumque voluptas modi dolorum.
+            Aliquid accusantium exercitationem ex dolorem quis quae, enim, porro
+            labore voluptatum ipsa animi sint quam id vero nesciunt unde.
+            Commodi, sapiente obcaecati ab, fuga consequatur voluptatibus hic
+            voluptatum cumque neque deleniti minima culpa reprehenderit
+            praesentium sint quam. Harum saepe dignissimos fugit aperiam eius
+            fugiat ipsum. Modi nulla dolorum voluptates. Reiciendis odit ut
+            nihil, tempore blanditiis quisquam laboriosam unde nisi rem, illo
+            rerum accusantium atque nobis voluptates laborum! Id voluptas
+            quaerat velit sed dolore eligendi voluptatem architecto obcaecati
+            nostrum itaque.
+          </p>
+        </div>
+        {/* menu */}
+        <div className="px-4 h-max sticky top-8">
+          <h1>Author</h1>
+          <div className="">
+            <Image
+              src="userImg.jpeg"
+              className="w-12 h-12 rounded-full object-cover"
+            />
+            <Link>Ibrahim Khalid</Link>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+            <div className="flex gap-2">
+              <Link>
+                <Image src="facebook.svg" />
+              </Link>
+              <Link>
+                <Image src="instagram.svg" />
+              </Link>
+            </div>
+          </div>
+          <PostMenuActions/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePostPage
+export default SinglePostPage;
